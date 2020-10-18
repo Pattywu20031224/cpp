ca3c77ba99d78c5b4ad8793707603d389eb2cedf
PeiYun <64179604+PeiWun@users.noreply.github.com>
2020-10-18 23:58:36 +0800

    Update main.cpp

--- 計算機/main.cpp
+++ 計算機/main.cpp
@@ -22,39 +22,39 @@ int main(){
                 cout<<"missing a"<<"q1"<<endl;
                 cout<<sqrt(input[5]*input[5]-input[3]*input[3]);
             }
-            if(missing==2){
+            else if(missing==2){
                 cout<<"missing b"<<"q1"<<endl;
                 cout<<sqrt(input[5]*input[5]-input[2]*input[2]);
             }
-            if(missing==3){
+            else{
                 cout<<"missing c"<<"q1"<<endl;
                 cout<<sqrt((input[2]*input[2])+(input[4]*input[4]));
             }
         }
-        if(input[0]==2){
+        else if(input[0]==2){
             if(missing==1){
                 cout<<"missing a"<<"q2"<<endl;
                 ans=input[5]/2 + input[3];
             }
-            if(missing==2){
+            else if(missing==2){
                 cout<<"missing b"<<"q2"<<endl;
                 ans=input[2]- input[5]/2 ;
             }
-            if(missing==3){
+            else{
                 cout<<"missing c"<<"q2"<<endl;
                 ans=(input[2]-input[4])*2;
             }
         }
-        if(input[0]==3){
+        else if(input[0]==3){
             if(missing==1){
                 cout<<"missing a"<<"q3"<<endl;
                 ans=input[5]/input[3];
             }
-            if(missing==2){
+            else if(missing==2){
                 cout<<"missing b"<<"q3"<<endl;
                 ans=input[5]/input[2];
             }
-            if(missing==3){
+            else{
                 cout<<"missing c"<<"q3"<<endl;
                 ans=input[2]*input[4];
             }
